22f320440bb955a7e7cd8477dedfc9df2a9e78e7
Emil Buraczynski <emil.buraczynski@softman.pl>
2023-03-18 14:10:18 +0100

    first commit

---
+++ src/main/java/com/example/demo/entity/EmployeeEntity.java
@@ -0,0 +1,53 @@
+package com.example.demo.entity;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class EmployeeEntity {
+
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "email")
+    private String email;
+
+    public EmployeeEntity() {
+    }
+
+    public EmployeeEntity(Long id, String username, String email) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ src/main/java/com/example/demo/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,44 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.entity.EmployeeEntity;
+import com.example.demo.repository.EmployeeRepository;
+import com.example.demo.service.EmployeeService;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+    @Override
+    public List<EmployeeEntity> findAllEmployye() {
+        return employeeRepository.findAll();
+    }
+
+    @Override
+    public Optional<EmployeeEntity> findById(Long id) {
+        return employeeRepository.findById(id);
+    }
+
+    @Override
+    public EmployeeEntity saveEmployee(EmployeeEntity employeeEntity) {
+        return employeeRepository.save(employeeEntity);
+    }
+
+    @Override
+    public EmployeeEntity updateEmployee(EmployeeEntity employeeEntity) {
+        return employeeRepository.save(employeeEntity);
+    }
+
+    @Override
+    public void deleteEmployee(Long id) {
+         employeeRepository.deleteById(id);
+    }
+}
